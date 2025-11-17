752b12799f01ae451405c5bf5e8d930d859f9e87
Jyotirmoy Halder <jyotirmoy.cse5.bu@gmail.com>
2025-11-17 11:58:25 +0600

    custom operator bootcamp

---
+++ SwiftfulThinkingAdvanceLearning2/CustomOperatorBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  CustomOperatorBootcamp.swift
+//  SwiftfulThinkingAdvanceLearning2
+//
+//  Created by jyotirmoy_halder on 17/11/25.
+//
+
+import SwiftUI
+
+struct CustomOperatorBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomOperatorBootcamp()
+}
